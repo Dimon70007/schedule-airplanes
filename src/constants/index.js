ae847b80ed590e49a8cb77841b4a5e64bfad62f7
Dimon70007 <dimon.fly.mail@mail.ru>
2017-08-05 05:07:57 +0300

    calendar_Treshold has been refactored

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,5 +6,5 @@ export {
 export const APP_WIDTH = window.innerWidth;
 export const CALENDAR_ITEM_WIDTH = 100;
 export const CALENDAR_ITEM_HEIGHT = 120;
-export const CALENDAR_LENGTH = 101;
+export const CALENDAR_LENGTH = 201;
 export const CALENDAR_TRESHOLD = Math.trunc(CALENDAR_LENGTH / 4);
