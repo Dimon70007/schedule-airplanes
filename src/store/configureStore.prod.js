25858a4e940e7c8e281afa171a0aed25fc4d1a72
Dimon70007 <dimon.fly.mail@mail.ru>
2017-08-11 03:24:49 +0300

    build without redux-logger

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,17 +1,16 @@
 
 import { createStore, applyMiddleware } from 'redux';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
 // import promisesMiddleware from '../middleware/promises';
 
 
-const logger = createLogger();
+// const logger = createLogger();
 const configureStore = preloadedState => createStore(
   reducers,
   preloadedState,
-  // logger пока оставил, чтоб видеть баги (для обучения)
-  applyMiddleware(thunk, logger /* , promisesMiddleware*/),
+  applyMiddleware(thunk /* , logger , promisesMiddleware*/),
 );
 
 export default configureStore;
